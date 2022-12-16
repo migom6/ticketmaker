48c88e9c990d785f24ea5afd4b200321d6ac6c3b
Madhurjya Pegu <migom6@gmail.com>
2022-12-17 00:34:11 +0530

    added loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
 import Image from "next/image";
 
-async function getPosts() {
-  return [];
-}
-
 export default async function Page() {
-  // Fetch data directly in a Server Component
-  const recentPosts = await getPosts();
-  // Forward fetched data to your Client Component
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex relative h-[500px] w-[360px] hover:animate-spin rounded-sm overflow-hidden shadow-md drop-shadow-sm">
